9594134aa77127fd3ce8b9f74ccf9807c3423c01
Tomche Delev <tomce.delev@gmail.com>
2018-05-16 22:23:34 +0200

    Fix undefined

--- js/wedding.js
+++ js/wedding.js
@@ -17,14 +17,49 @@ $(document).ready(function () {
     });
 
     var invited = {
+        // Tomche
+        "im": "Иван и Маја",
+        "ai": "Аце и Ивана",
+        "bt": "Борче и Тамара",
+        "ls": "Лазе и Спаска",
+        "ao": "Александар и Олга",
+        "bk": "Блаже и Катерина",
         "rs": "Ристе и Слободанка",
-        "bk": "Блаже и Катерина"
+        "z": "Зоки",
+        "j": "Јоки",
+        "ga": "Ѓорѓи и Анета",
+        "iz": "Иван и Зорица",
+        "kb": "Катарина и Бојан",
+        "aa": "Александра и Александар",
+        "ad": "Александра и Дејан",
+        "ek": "Ефтим и Катерина",
+        "pe": "Петре и Елена",
+        "ha": "Христина и Атанас",
+        "ii": "Ивица и Ивана",
+        "dg": "Дејан",
+        "ic": "Иван Чорбев",
+        "vv": "Владимир и Валерија",
+        "is": "Игор и Сања",
+        "dm": "Дионис и Мариела",
+        // Nadica
+        /*"ad": "Маја и Коста",
+        "ad": "Тијана и Трајче",
+        "ad": "Доне и Славица",
+        "ad": "Анастасија и Антоние",
+        "ad": "Ивана и Диме",
+        "ad": "Диме",
+        "ad": "Стефан",
+        "ad": "Дениз",
+        "ad": "Тоше и Марија",
+        "ad": "Игор",
+        "ad": "Ивана",
+        "ad": "Александра и Дејан",*/
     };
 
     var key = getParameterByName('who');
     // console.log(key);
     // console.log(invited[key]);
-    var guest = invited[key];
+    var guest = invited[key] || "";
     var formUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSeBVtaqr3Zodpko414m1VirgYnCTxLlWCwh4jceJcgKwZyEQA/viewform?embedded=true';
     formUrl = formUrl + '&entry.229359151=' + guest;
     $('#google-form').attr('src', formUrl);
